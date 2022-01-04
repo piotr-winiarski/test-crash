b1bf5b33638a07e5fffb55e171dc3ea116fbc13a
piotr-winiarski <62816626+piotr-winiarski@users.noreply.github.com>
2022-01-04 22:33:55 +0100

    removed methods

--- main.ts
+++ main.ts
@@ -1,28 +1,9 @@
-function doSomething3 () {
-    for (let value of list) {
-        tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
-    }
-    game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
-}
 function doSomething () {
     for (let value of tiles.getTilesByType(sprites.castle.tileDarkGrass2)) {
         tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
     }
     game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 }
-function doSomething2 () {
-    for (let value of list) {
-        tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
-    }
-    game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
-}
-function doSomething4 () {
-    for (let value of list) {
-        tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
-    }
-    game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
-}
-let list = 0
 let mySprite: Sprite = null
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
