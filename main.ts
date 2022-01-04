bf40e727304170d958ba5544eda977c6b862b6f7
piotr-winiarski <62816626+piotr-winiarski@users.noreply.github.com>
2022-01-04 22:44:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,6 +17,27 @@ function doSomething2 () {
     game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 }
 function doSomething4 () {
+    game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
+    tiles.setTilemap(tilemap`level1`)
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 3 . . . . . 3 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . 3 3 . . . . . . . . . . . 
+        . . . . 3 . . . . . . . . . . . 
+        . . . . . 3 3 . . . . . . . . . 
+        . . . . . . . 3 . . . . . . . . 
+        . . . . . . . . 3 3 . . . . . . 
+        . . . . . . . . . . 3 3 . . . . 
+        . . . . . . . . . . . . 3 3 3 3 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    doSomething()
     for (let value of tiles.getTilesByType(sprites.castle.tileDarkGrass2)) {
         tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
     }
@@ -25,24 +46,3 @@ function doSomething4 () {
 let mySprite: Sprite = null
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
-game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
-tiles.setTilemap(tilemap`level1`)
-mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . 3 . . . . . 3 . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . 3 3 . . . . . . . . . . . 
-    . . . . 3 . . . . . . . . . . . 
-    . . . . . 3 3 . . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . . 3 3 . . . . . . 
-    . . . . . . . . . . 3 3 . . . . 
-    . . . . . . . . . . . . 3 3 3 3 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-doSomething()
