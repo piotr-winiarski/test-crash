df09eab8b28e6def239199937f238eafab18cdcf
piotr-winiarski <62816626+piotr-winiarski@users.noreply.github.com>
2022-01-04 22:30:47 +0100

    dodanie place mySprite on top of random

--- main.ts
+++ main.ts
@@ -1,7 +1,31 @@
 function doSomething () {
+    let list: number[] = []
+    for (let value of list) {
+        tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
+    }
     game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 }
+let mySprite: Sprite = null
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
+tiles.setTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 3 . . . . . 3 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . 3 3 . . . . . . . . . . . 
+    . . . . 3 . . . . . . . . . . . 
+    . . . . . 3 3 . . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . . 3 3 . . . . . . 
+    . . . . . . . . . . 3 3 . . . . 
+    . . . . . . . . . . . . 3 3 3 3 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 doSomething()
