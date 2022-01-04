727b9df6f665be6a28f6d4ab993c673895a028bb
piotr-winiarski <62816626+piotr-winiarski@users.noreply.github.com>
2022-01-04 22:31:11 +0100

    zduplikowane funkcje

--- main.ts
+++ main.ts
@@ -1,10 +1,28 @@
-function doSomething () {
-    let list: number[] = []
+function doSomething3 () {
     for (let value of list) {
         tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
     }
     game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 }
+function doSomething () {
+    for (let value of list) {
+        tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
+    }
+    game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
+}
+function doSomething2 () {
+    for (let value of list) {
+        tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
+    }
+    game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
+}
+function doSomething4 () {
+    for (let value of list) {
+        tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
+    }
+    game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
+}
+let list = 0
 let mySprite: Sprite = null
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
 game.showLongText("śćźńłó co tam co tam", DialogLayout.Bottom)
